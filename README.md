b0994935c63a2b52caaaf4ccd34666f359b06c9b
dragosguta <contact@dragosguta.com>
2016-02-23 21:36:35 -0800

    Initialize repo

---
+++ README.md
@@ -0,0 +1,4 @@
+blog
+====
+
+A Symfony project created on February 24, 2016, 5:11 am.
---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", name="blog_home")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
